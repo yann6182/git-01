02d3c81fc832c78e45c37cd07706978b458ab0ef
Regis <regis@regis.com>
2025-02-26 11:33:14 +0100

    feat: draw something

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
     "strings"
 )
 
-
 func captureSigint() {
     c := make(chan os.Signal, 1)
     signal.Notify(c, os.Interrupt)
@@ -44,26 +43,22 @@ func main() {
     const outputChar = "  "
 
     // Set colors
-    // Set colors
-// Set colors
-colors := map[string]string{
-    "#": "82",   // green
-    "@": "223",  // light orange
-    "-": "205",  // pink
-    "$": "219",  // light pink
-    "%": "217",  // peach
-    "=": "39",   // blue
-    ">": "196",  // red
-    "+": "226",  // yellow
-    ".": "17",   // deep blue
-    ",": "15",   // white
-    ":": "09",   // purple
-    "&": "214",  // orange
-    "0": "0",    // black
-    "*": "245",  // grey
-}
-
-  
+    colors := map[string]string{
+        "#": "82",      // green
+        "@": "223",     // light orange
+        "-": "205",     // pink
+        "$": "219",     // light pink
+        "%": "217",     // peach
+        "=": "39",      // blue
+        ">": "196",     // red
+        "+": "226",     // yellow
+        ",": "17",      // deep blue
+        ".": "15",      // white
+        ";": "99",      // purple
+        "&": "214",     // orange
+        "'": "0",       // black
+        "*": "245",     // grey
+    }
 
     // Import frames from data file
     framesFile, _ := filepath.Abs("assets/frames.json")
